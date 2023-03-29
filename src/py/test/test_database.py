ded6e6feb6458a0cded0e6c4a3e05138ac87e4f4
Benjamin Oshana <bo562@uowmail.edu.au>
2023-03-30 00:58:08 +1100

    Database Handler Class
    Completed Database class, allowing for quick connection to database. Containing management functions that make connecting, querying easier. (To do is implement better result management and simplification of querying)

---
+++ src/py/test/test_database.py
@@ -0,0 +1,42 @@
+import unittest
+from util.database import Database as db, DatabaseStatus as ds
+
+secret_name = '314-db-lambda-user'
+
+
+class TestDatabase(unittest.TestCase):
+    def test_database_connection(self):
+        database = db.Database.database_handler(secret_name)  # create database to connect to
+        database.database_connect()  # connect to database
+        self.assertEqual(database.status, ds.DatabaseStatus.Connected)
+
+    def test_database_query(self):
+        database = db.Database.database_handler(secret_name)  # create database to connect to
+        database.database_connect()  # connect to database
+
+        # check if connected to database
+        if database.status is not ds.DatabaseStatus.Connected:
+            raise Exception()
+
+        # run query to database
+        query = "SELECT * FROM User WHERE user_id = %s"
+        user_id = "1"
+        result = database.database_query(query, tuple(user_id))
+
+    def test_database_query1(self):
+        database = db.Database.database_handler(secret_name)  # create database to connect to
+        database.database_connect()  # connect to database
+
+        # check if connected to database
+        if database.status is not ds.DatabaseStatus.Connected:
+            raise Exception()
+
+        # run query to database
+        query = "SELECT * FROM User"
+        result = database.database_query(query)
+
+    def tearDown(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- src/py/util/database/Database.py
+++ src/py/util/database/Database.py
@@ -1,4 +1,4 @@
-from mysql.connector.locales.eng import client_error # for compilation, no real use in code
+from mysql.connector.locales.eng import client_error  # for compilation, no real use in code
 from mysql.connector.errors import Error
 from mysql.connector import errorcode, MySQLConnection, connect
 from botocore.exceptions import ClientError
@@ -7,15 +7,15 @@ import boto3
 import json
 
 
-class Database():
+class Database:
     def __init__(self, host, database, user, password):
         self.status: DatabaseStatus = NotImplemented
 
         # creating mysql connection
-        self.database_connection = MySQLConnection(user=user, password=password, host=host, database='LambdaTest')
+        self.__database_connection = MySQLConnection(user=user, password=password, host=host, database=database)
 
         # check database connection status
-        if self.database_connection.is_connected():
+        if self.__database_connection.is_connected():
             self.status = DatabaseStatus.Connected
         else:
             self.status = DatabaseStatus.Disconnected
@@ -34,11 +34,10 @@ class Database():
 
         # attempt database connection
         try:
-            database = Database(mysql_info['host'], 'Project',
+            database = Database(mysql_info['host'], mysql_info['database'],
                                 mysql_info['username'], mysql_info['password'])
-            database.database_connect()
         except Error as err:
-            return err
+            raise err
 
         return database
 
@@ -46,34 +45,33 @@ class Database():
     def get_secret(secret_name):
         region_name = "us-east-1"
 
-        # Create a Secrets Manager client
+        # create a Secrets Manager client
         session = boto3.session.Session()
         client = session.client(
             service_name='secretsmanager',
             region_name=region_name
         )
 
+        # secret response
         try:
-            get_secret_value_response = client.get_secret_value(
-                SecretId=secret_name
-            )
+            get_secret_value_response = client.get_secret_value(SecretId=secret_name)
         except ClientError as e:
             raise e
 
-        # Decrypts secret using the associated KMS key.
+        # decrypts secret using the associated KMS key.
         secret = get_secret_value_response['SecretString']
 
         return secret
 
     def database_connect(self):
-        if not self.database_connection.is_connected():
-            self.database_connection.connect()
+        if not self.__database_connection.is_connected():
+            self.__database_connection.connect()
             self.status = DatabaseStatus.Connected
 
     def database_disconnect(self):
-        if self.database_connection.is_connected():
-            self.database_connection.disconnect()
-            self.database_connection.close()
+        if self.__database_connection.is_connected():
+            self.__database_connection.close()
+            self.__database_connection.disconnect()
         self.status = DatabaseStatus.Disconnected
 
     def database_select(self, table):
@@ -88,11 +86,13 @@ class Database():
     def database_delete(self):
         pass
 
-    def database_query(self, query):
-        cursor = self.database_connection.cursor()
+    def database_query(self, query, args: tuple = None):
+        cursor = self.__database_connection.cursor()
         try:
-            cursor.execute(query)
+            # attempt to run query and return results
+            cursor.execute(query, args)
+            result = cursor.fetchall()
         except Error as err:
             raise err
 
-        print(cursor.fetchall())
+        return result
