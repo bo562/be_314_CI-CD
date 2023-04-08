29c11b230be771ecadd09c67f7816eafb7684461
bdurkin2 <bdurkin2>
2023-04-08 14:34:03 +1000

    Added request table

---
+++ src/database/CreateRequest.sql
@@ -0,0 +1,14 @@
+CREATE TABLE request (
+	 request_id         MEDIUMINT NOT NULL AUTO_INCREMENT,
+     client_id          MEDIUMINT NOT NULL,
+     service_id         MEDIUMINT NOT NULL,
+     request_date       DATETIME NOT NULL,
+     professional_id    MEDIUMINT,
+     start_date         DATETIME,
+     completion_date    DATETIME,
+     instruction        VARCHAR(4000),
+     PRIMARY KEY (request_id),
+     FOREIGN KEY (client_id) REFERENCES client(client_id),
+     FOREIGN KEY (professional_id) REFERENCES client(professional_id),
+     FOREIGN KEY (service_id) REFERENCES service(service_id)
+);
