046c823b80e90ce11a47476bb06a276f2eab0e2b
bdurkin2 <bdurkin2>
2023-04-09 11:12:18 +1000

    Added table

---
+++ src/database/request_bid.sql
@@ -0,0 +1,25 @@
+CREATE TABLE request_bid (
+	 request_bid_id               MEDIUMINT NOT NULL AUTO_INCREMENT,
+     request_id                   MEDIUMINT NOT NULL,
+     professional_id              MEDIUMINT NOT NULL,
+     sent_date                    DATETIME NOT NULL,
+     accepted_by_client_date      DATETIME,
+     professional_cancelled_date  DATETIME,     
+     bid_status_id                MEDIUMINT NOT NULL,
+     PRIMARY KEY (request_bid_id),
+     FOREIGN KEY (professional_id) REFERENCES client(professional_id),
+     FOREIGN KEY (request_id) REFERENCES request(request_id)
+     FOREIGN KEY (bid_status_id) REFERENCES bid_status(bid_status_id)
+);
+
+CREATE TABLE bid_status (
+	 bid_status_id               MEDIUMINT NOT NULL,
+     status_name                 VARCHAR(100) NOT NULL,
+     PRIMARY KEY (bid_status_id),
+);
+
+INSERT INTO bid_status (bid_status_id, status_name) values (1, 'Active');
+INSERT INTO bid_status (bid_status_id, status_name) values (2, 'Accepted');
+INSERT INTO bid_status (bid_status_id, status_name) values (3, 'Rejected');
+INSERT INTO bid_status (bid_status_id, status_name) values (4, 'Cancelled');
+COMMIT;
