132efa036eebfe3c959adecd7fed8f113a051a59
bdurkin2 <bdurkin2>
2023-04-09 11:25:52 +1000

    split files

--- src/database/CreateRequest.sql
+++ src/database/CreateRequest.sql
@@ -13,34 +13,3 @@ CREATE TABLE request (
      FOREIGN KEY (professional_id) REFERENCES client(professional_id),
      FOREIGN KEY (service_id) REFERENCES service(service_id)
 );
-
-CREATE TABLE request_status (
-	 request_status_id           MEDIUMINT NOT NULL,
-     status_name                 VARCHAR(100) NOT NULL,
-     PRIMARY KEY (request_status_id),
-);
-CREATE UNIQUE INDEX uc_request_status_name ON request_status(status_name);
-
-INSERT INTO bid_status (request_status_id, status_name) values (1, 'Open');
-INSERT INTO bid_status (request_status_id, status_name) values (2, 'Assigned');
-INSERT INTO bid_status (request_status_id, status_name) values (3, 'In progress');
-INSERT INTO bid_status (request_status_id, status_name) values (3, 'Completed - not paid');
-INSERT INTO bid_status (request_status_id, status_name) values (3, 'Completed');
-INSERT INTO bid_status (request_status_id, status_name) values (4, 'Cancelled');
-COMMIT;
-
-CREATE TABLE request_transaction (
-	 request_id           MEDIUMINT NOT NULL,
-     transaction_id       MEDIUMINT NOT NULL,
-     PRIMARY KEY (request_id, transaction_id),
-     FOREIGN KEY (request_id) REFERENCES request(request_id),
-     FOREIGN KEY (transaction_id) REFERENCES transaction(transaction_id)
-);
-
-CREATE TABLE request_subscription (
-	 request_id            MEDIUMINT NOT NULL,
-     subscription_id       MEDIUMINT NOT NULL,
-     PRIMARY KEY (request_id, subscription_id),
-     FOREIGN KEY (request_id) REFERENCES request(request_id),
-     FOREIGN KEY (subscription_id) REFERENCES subscription(subscription_id)
-);
