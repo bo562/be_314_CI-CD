40ed2980041902dbc3dedad459ce1292f9f2ff2d
bdurkin2 <bdurkin2>
2023-04-10 12:37:51 +1000

    refactor

--- src/database/all.sql
+++ src/database/all.sql
@@ -233,6 +233,22 @@ CREATE TABLE bid_status (
 CREATE UNIQUE INDEX uc_bid_status_name ON bid_status(status_name);  
   
 ------------------------------ 
+-- From: 190_request_bid.sql 
+CREATE TABLE request_bid (
+     request_bid_id               MEDIUMINT NOT NULL AUTO_INCREMENT,
+     request_id                   MEDIUMINT NOT NULL,
+     professional_id              MEDIUMINT NOT NULL,
+     sent_date                    DATETIME NOT NULL,
+     accepted_by_client_date      DATETIME,
+     professional_cancelled_date  DATETIME,     
+     bid_status_id                MEDIUMINT NOT NULL,
+     PRIMARY KEY (request_bid_id),
+     FOREIGN KEY (professional_id) REFERENCES client(professional_id),
+     FOREIGN KEY (request_id) REFERENCES request(request_id)
+     FOREIGN KEY (bid_status_id) REFERENCES bid_status(bid_status_id)
+);  
+  
+------------------------------ 
 -- From: 300_insert_billing_type.sql 
 INSERT INTO billing_type (billing_type_name,retired) VALUES ('Out', null);
 
