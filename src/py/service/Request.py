7ad353961a515ddb721f6859dc731058a0d517c0
rate9outof111 <116358037+rate9outof111@users.noreply.github.com>
2023-04-14 22:03:09 +0930

    Update Request.py
    Client Service Request implementation

--- src/py/service/Request.py
+++ src/py/service/Request.py
@@ -3,6 +3,10 @@ py that handles data for requests type
 """
 from datetime import datetime
 from dataclasses import dataclass
+from util.database.DatabaseLookups import DatabaseLookups
+from util.database.Database import Database
+from util.database.DatabaseStatus import DatabaseStatus
+from mysql.connector import errors
 
 
 @dataclass()
@@ -15,12 +19,81 @@ class Request:
     client_id: int = None
     professional_id: int = None
     service_id: int = None
+    request_status_id: int = None
+    status_name: str = None
 
     def create_request(self):
-        pass
+        database = Database.database_handler(DatabaseLookups.User)  # create database connection
+
+        # check if database is connected, if not connect
+        if database.status is DatabaseStatus.Disconnected:
+            database.connect()
+
+        elif database.status is DatabaseStatus.NoImplemented:
+            raise errors.InternalError  # change with mysql errors
+            # attempt to create address row
+        database.select(('request_status_id',), 'request_status')
+        database.where('status_name = %s', self.status_name)
+        self.request_status_id = database.run()[0][0]
+        database.clear()
+
+        # attempt to create address row
+        database.insert(self, 'request', ('request_id', 'professional_id', 'completion_date', 'request_status_id', 'status_name',))
+
+        try:
+            addressid = database.run()
+            database.commit()
+        except errors.IntegrityError as ie:  # in case that user already exists
+            if ie.errno == 1452:  # cannot solve gracefully
+                raise ie
+
+            # constructing query to return already created user
+            database.clear()
+            database.select(('address_id',), 'address')
+            database.where('user_id = %s', self.user_id)
+            address_id = database.run()[0][0]
+
+        self.address_id = address_id
+
+        # clear database tool
+        database.clear()
+
+        return self
 
     def update_request(self):
-        pass
+        database = Database.database_handler(DatabaseLookups.User)  # create database connection
+
+        # check if database is connected, if not connect
+        if database.status is DatabaseStatus.Disconnected:
+            database.connect()
+
+        elif database.status is DatabaseStatus.NoImplemented:
+            raise errors.InternalError  # change with mysql errors
+        if self.status_name is not None:
+            try:
+                database.select(('request_status_id',), 'request_status')
+                database.where('status_name = %s', self.status_name)
+                self.request_status_id = database.run()[0][0]
+                database.clear()
+            except errors.IntegrityError as ie:  # in case that user already exists
+                if ie.errno == 1452:  # cannot solve gracefully
+                    raise ie
+        # create query
+        database.clear()
+        database.update(self, 'request', ('request_id', 'status_name'))
+        database.where('request_id = %s', self.request_id)
+
+        try:  # attempt to return
+            database.run()
+            database.commit()
+
+        except errors.IntegrityError as ie:
+            raise ie
+
+        # clear database tool
+        database.clear()
+
+        return self
 
     def get_request(self):
         pass
