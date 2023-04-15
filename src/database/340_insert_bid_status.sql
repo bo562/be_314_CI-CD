f38fbc4133b0866127121806e5d88e831af208a0
Benjamin Oshana <bo562@uowmail.edu.au>
2023-04-15 22:05:27 +1000

    Updated based on comment

--- src/database/340_insert_bid_status.sql
+++ src/database/340_insert_bid_status.sql
@@ -1,9 +1,7 @@
-INSERT INTO bid_status (bid_status_id, status_name) values (1, 'Active');
+INSERT INTO bid_status (status_name) values (1, 'Approved');
 
-INSERT INTO bid_status (bid_status_id, status_name) values (2, 'Accepted');
+INSERT INTO bid_status (status_name) values ('Declined');
 
-INSERT INTO bid_status (bid_status_id, status_name) values (3, 'Rejected');
-
-INSERT INTO bid_status (bid_status_id, status_name) values (4, 'Cancelled');
+INSERT INTO bid_status (status_name) values (3, 'Pending');
 
 COMMIT;
